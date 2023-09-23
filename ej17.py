c2a638c9e388544130d08dccd51dc39e9c749401
Promete04 <ramossantosguillermo14@gmail.com>
2023-09-23 10:47:56 +0200

    create files

---
+++ ej17.py
@@ -0,0 +1,13 @@
+a=False
+
+a=a==False
+print(a)
+
+"""Compara el valor de a respecto a de False. Si son el mismo, dará como resultado true y en el caso contrario false"""
+
+a=True
+
+a=a==False
+print(a)
+
+"""Si no inicializamos previamente "a"  nos dara un error----> NameError: name 'a' is not defined"""
