ffb7e7fd144ac7fa972aeab8257694643fdcbd4b
Promete04 <ramossantosguillermo14@gmail.com>
2023-09-29 11:35:05 +0200

    modify files

--- clase.py
+++
@@ -1,9 +0,0 @@
-import math
-
-def abs(x:float) -> float:
-    if x>0:
-        return x
-    else:
-        return -x
-               
-print(abs(-899))
---
+++ clase2.py
@@ -0,0 +1,42 @@
+"""import math
+
+def abs(x:float) -> float:
+    if x>0:
+        return x
+    else:
+        return -x
+               
+print(abs(-899))
+
+a=1  
+b=2
+
+print(a)   #Si queremos intercambiar las etiquetas 
+print(b)   #no vale poner a=b y b=a
+
+aux = a
+a= b
+b = aux
+
+print(a)
+print(b)
+
+def swap(z,s):
+    aux=z
+    z=s
+    s=aux
+    return (z,s)
+
+menudo paripé
+
+print(swap(6,5))
+
+a,b = swap(a,b)
+
+Todo para saber que es mejor a,b=b,a"""
+
+
+
+
+
+
