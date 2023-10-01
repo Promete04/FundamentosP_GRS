37eacd69215ef760f5f61e04c44a0b94b1a59600
Promete04 <ramossantosguillermo14@gmail.com>
2023-10-01 11:16:14 +0200

    modify files

--- Cuaderno1/ej14.py
+++ Cuaderno1/ej14.py
@@ -1,3 +1,16 @@
+""". En Python es posible hacer operaciones con variables de tipo str. Vamos a
+probarlo tecleando:
 fruta = " ciruela "
 tipo = " claudia "
 print (fruta + tipo)
+a) ¿Qué se obtiene? ¿Qué es lo que hace la operación + con las cadenas de
+texto?
+Teclea ahora:
+print fruta * 3
+b) ¿Qué se obtiene? ¿Qué hace la operación * con los valores tipo texto?"""
+
+fruta = " ciruela "
+tipo = " claudia "
+print (fruta + tipo)
+
+print(fruta*3)
--- Cuaderno1/ej18.py
+++ Cuaderno1/ej18.py
@@ -1,10 +1,13 @@
+"""Haz la tabla de verdad de p and not q or r teniendo en cuenta el orden de
+prelación de los operadores en Python. """
+
 si_o_no = [True,False]
 
-print('p\tq\tr\tp nand q or r\t')   #creamos la parte desuperior de la tabla con los datos
-print('-'*37)   #colocamos la barra
+print("p\tq\tr\tp nand q or r\t")   #creamos la parte superior de la tabla con los datos
+print("-"*37)   #colocamos la barra
 for p in si_o_no:
     for q in si_o_no:
         for r in si_o_no:
-            print(p, q, r, p and not q or r, sep = '\t')
+            print(p, q, r, p and not q or r, sep = "\t")
 
     
