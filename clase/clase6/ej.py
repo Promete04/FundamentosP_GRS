7c40f5ef9b4912afcb4990b91f57f1d527d3bcaf
Promete04 <ramossantosguillermo14@gmail.com>
2023-11-10 11:44:27 +0100

    create files

---
+++ clase/clase6/ej.py
@@ -0,0 +1,16 @@
+import random
+def encuentra_secuencia(seq,times)-> bool:
+    
+    ant= seq[0]
+    nveces = 1
+    siguiente_idx= 1
+    while nveces < times and siguiente_idx < len (seq):
+        if seq[siguiente_idx] == ant:
+             nveces +=1
+        else:
+            nveces = 1
+        ant = seq[siguiente_idx]
+        siguiente_idx = siguiente_idx+1
+    return (siguiente_idx - times, True) if nveces == times else (None, False)
+
+print(encuentra_secuencia([0,5,5,5,6,9,87,5,5,5,4,10],3))
